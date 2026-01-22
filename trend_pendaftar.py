5be82f7b75774744c2fa10624a609b6a91a8b80a
Onky Prisma Pradana <freddycull27@gmail.com>
2026-01-22 17:41:01 +0700

    trend_pendaftar.py

--- trend_pendaftar.py
+++ trend_pendaftar.py
@@ -16,3 +16,22 @@ st.set_page_config(
 df = pd.read_csv("data_pendaftar_clean.csv")
 
 st.write(df.columns)
+
+df["Tanggal Gabungan"] = pd.to_datetime(df["Tanggal Gabungan"])
+st.header("📈 Tren Pendaftar Bulanan")
+
+trend = (
+    df.groupby("period")
+    .size()
+    .reset_index(name="Jumlah Pendaftar")
+)
+
+fig, ax = plt.subplots(figsize=(10, 4))
+ax.plot(trend["period"], trend["Jumlah Pendaftar"], marker="o")
+
+ax.set_xlabel("Bulan")
+ax.set_ylabel("Jumlah Pendaftar")
+ax.set_title("Tren Pendaftar Bulanan")
+
+plt.xticks(rotation=45)
+st.pyplot(fig)
